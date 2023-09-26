faa421dcca0de127ab8f2bd58988ce8fdef9b4a4
DaniMariagesUPC <99950347+DaniMariages@users.noreply.github.com>
2023-09-26 18:43:14 +0200

    ModuleEditor.cpp and .h

---
+++ EXP_Engine_Sol/EXP_Engine_Sol/ModuleEditor.cpp
@@ -0,0 +1,58 @@
+#include "Globals.h"
+#include "Application.h"
+#include "ModuleEditor.h"
+#include "ModuleWindow.h"
+#include "ImGui/imgui.h"
+#include "ImGui/backends/imgui_impl_opengl3.h"
+#include "ImGuI/backends/imgui_impl_sdl2.h"
+
+ModuleEditor::ModuleEditor(Application* app, bool start_enabled) : Module(app, start_enabled)
+{
+}
+
+// Destructor
+ModuleEditor::~ModuleEditor ()
+{
+}
+
+// Called before render is available
+bool ModuleEditor::Init()
+{
+	bool ret = true;
+
+	return ret;
+}
+
+void ModuleEditor :: DrawEditor()
+{
+	//Start the Dear InGui frame
+	ImGui_ImplOpenGL3_NewFrame();
+	ImGui_ImplSDL2_NewFrame();
+	ImGui :: NewFrame();
+
+	if (ImGui::BeginMainMenuBar())
+	{
+		if (ImGui::BeginMenu("File"))
+		{
+			ImGui::Text("Hello world!");
+			ImGui::EndMenu();
+		}
+		ImGui::EndMainMenuBar();
+	}
+
+	if (ImGui::Begin("Configuratiobn"))
+	{
+		ImGui::PlotHistogram("FPS", mFPSLog.data(), mFPSLog.size());
+	}
+
+	ImGui::ShowDemoWindow();
+
+	ImGui :: Render();
+	ImGui_ImplOpenGL3_RenderDrawData(ImGui :: GetDrawData());
+}
+
+// Called before quitting
+bool ModuleEditor::CleanUp()
+{
+	return true;
+}
