a4dc1f293d72329b3b36ca29338b5133ea28d9f3
Toni Romanos Ruiz <antonio.romanos@estudiantat.upc.edu>
2023-10-09 10:07:16 +0200

    Load Geometry changes

---
+++ EXP_Engine_Sol/EXP_Engine_Sol/Mesh.h
@@ -0,0 +1,7 @@
+#pragma once
+
+#include "Assimp/include/cimport.h"
+#include "Assimp/include/scene.h"
+#include "Assimp/include/postprocess.h"
+#pragma comment (lib, "Assimp/libx86/assimp.lib")
+
--- EXP_Engine_Sol/EXP_Engine_Sol/ModuleLoadGeometry.h
+++ EXP_Engine_Sol/EXP_Engine_Sol/ModuleLoadGeometry.h
@@ -3,6 +3,7 @@
 #include "Assimp/include/cimport.h"
 #include "Assimp/include/postprocess.h"
 #include "Assimp/include/scene.h"
+using namespace std;
 
 #pragma comment (lib, "Assimp/libx86/assimp.lib")
 
@@ -12,5 +13,44 @@ public:
 	ModuleLoadGeometry(Application* app, bool start_enabled = true);
 	~ModuleLoadGeometry();
 
+	bool Init();
+	update_status PreUpdate(float dt);
+	update_status PostUpdate(float dt);
+	bool CleanUp();
+
+	void LoadFile();
+	void LoadMeshes();
+
+	const char* file_path = "C:/Users/antoniorr14/Documents/GitHub/Motors/EXP_Engine_Sol/EXP_Engine_Sol/Assets_FBX/warrior.FBX";
+	size_t size = 0;
+	
+private:
+	struct Vertex {
+		vec3 Position;
+		vec3 Normal;
+		vec2 TexCoords;
+	};
+
+	struct Texture {
+		unsigned int id;
+		std::string type;
+	};
+
+	class Mesh {
+	public:
+		// mesh data
+		vector<Vertex>   vertices;
+		vector<unsigned int> indices;
+		vector<Texture>      textures;
+
+		Mesh(vector<Vertex> vertices, vector<unsigned int> indices, vector<Texture> textures);
+		//void Draw(Shader& shader);
+
+	private:
+		//  render data
+		unsigned int VAO, VBO, EBO;
+
+		void setupMesh();
+	};
 
 };
